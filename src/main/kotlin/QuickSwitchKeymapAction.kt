671b700ef546586b9011dcd3d0ac30b4aa0e1a28
Tobias Karger <info@karger.it>
2019-01-07 20:30:18 +0100

    show message on quick switch

--- src/main/kotlin/QuickSwitchKeymapAction.kt
+++ src/main/kotlin/QuickSwitchKeymapAction.kt
@@ -1,9 +1,15 @@
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataKeys
 import com.intellij.openapi.keymap.KeymapManager
 import com.intellij.openapi.keymap.ex.KeymapManagerEx
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.wm.WindowManager
+import com.intellij.ui.awt.RelativePoint
+
 
 class QuickSwitchKeymapAction : AnAction() {
 
@@ -19,8 +25,15 @@ class QuickSwitchKeymapAction : AnAction() {
             val keymap = keymapManager.getKeymap(recentKeymap)
             keymapManager.activeKeymap = keymap
 
-            val project = event.getProject()
-            Messages.showMessageDialog(project, "Toggled to: " + recentKeymap, "Greeting", Messages.getInformationIcon())
+            val statusBar = WindowManager.getInstance()
+                    .getStatusBar(DataKeys.PROJECT.getData(event.getDataContext()))
+
+            JBPopupFactory.getInstance()
+                    .createHtmlTextBalloonBuilder("Switched Keymap to: " + recentKeymap, MessageType.INFO, null)
+                    .setFadeoutTime(1500)
+                    .createBalloon()
+                    .show(RelativePoint.getCenterOf(statusBar.component),
+                            Balloon.Position.atRight)
         }
     }
 }
